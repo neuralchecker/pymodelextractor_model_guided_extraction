f4c75942a4b8ae8795c4ffeaa5415fbbbf1537b2
juan pedro da silva <juanpedrodasilvabarloco@gmail.com>
2024-04-13 13:50:40 -0300

    Modified bert sequence probability calculations

--- case_studies/bert_small/bert_small_probabilistic_model_wrapper.py
+++ case_studies/bert_small/bert_small_probabilistic_model_wrapper.py
@@ -43,7 +43,7 @@ class BERT_SMALL_probabilistic_model_wrapper(ProbabilisticModel):
         return prob
     
     def get_last_token_weights(self, sequence, symbols):
-        return [weight for weight in self._get_probability([str(sequence)], symbols, False)]
+        return self._get_probability([str(sequence)], symbols)
     
     def get_last_token_weights_batch(self, sequences, required_suffixes):
         results = []
@@ -54,37 +54,35 @@ class BERT_SMALL_probabilistic_model_wrapper(ProbabilisticModel):
     def tokenize(self, sequence: str):
         return self.tokenizer(sequence, return_tensors="pt").to(self.device)
 
-    def _get_probability(self, sequence: str, symbols: list, normalize: bool = True):
+    def _get_probability(self, sequence: str, symbols: list):
         tokens = self.tokenize(sequence)
+        token_ids = tokens.input_ids[:, :-1]
 
         with torch.no_grad():
-            output = self.model(tokens.input_ids)
+            output = self.model(token_ids)
             logits = output.logits
             probs = torch.softmax(logits, dim=-1)
 
         word_probabilities = []
         for word in symbols:
             word_tokens = self.tokenize(str(word))
-            
-            #transform tensor to list
-            word_tokens.input_ids = word_tokens.input_ids.tolist()
 
             #remove the first and last token
-            word_tokens.input_ids = [i[1:-1] for i in word_tokens.input_ids]
+            word_tokens.input_ids = word_tokens.input_ids[:, 1:-1]
 
-            #get the probability of the word
-            word_probs = probs[0, -1, word_tokens.input_ids[-1]]
-            
-            total_word_probs = sum(word_probs)
-            total_word_probs /= len(word_probs)
-            word_probabilities.append(total_word_probs.item())
-
-
-        if normalize:
-            words = np.array(word_probabilities)
-            words /= np.sum(words)
-            word_probabilities = words.tolist()
-
+            word_probs = probs[0, -1, word_tokens.input_ids[-1]][0]
+            amount_of_tokens_for_last_symbols = len(word_tokens.input_ids[0])
+            if (amount_of_tokens_for_last_symbols > 1):
+                symbol_word_probs = 1
+                actual_sequence = token_ids
+                with torch.no_grad():
+                    for token in word_tokens.input_ids[0]:
+                        new_probs = torch.softmax(self.model(actual_sequence).logits, dim=-1)
+                        symbol_word_probs *= new_probs[0, -1, token]
+                    actual_sequence = torch.cat((actual_sequence, token.reshape((1,1))), dim=-1)
+                word_probs = symbol_word_probs
+            word_probabilities.append(word_probs)
+        
         return word_probabilities
 
     
